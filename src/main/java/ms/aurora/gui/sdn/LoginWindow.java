6663ddbaea87739277fd34bed33a4e7b8d4b71e2
Tobie Warburton <tobie.warburton@gmail.com>
2013-04-23 22:05:00 +0100

    start of javafx login

---
+++ src/main/java/ms/aurora/gui/sdn/LoginWindow.java
@@ -0,0 +1,72 @@
+package ms.aurora.gui.sdn;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import ms.aurora.sdn.net.api.Authentication;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author tobiewarburton
+ */
+public class LoginWindow {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnAuthenticate;
+
+    @FXML
+    private Label lblInfo;
+
+    @FXML
+    private PasswordField pwdPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    public LoginWindow() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginWindow.fxml"));
+
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    void onAuthenticate(ActionEvent event) {
+        String username = txtUsername.getText();
+        String password = pwdPassword.getText();
+        btnAuthenticate.setDisable(true);
+        Authentication.login(username, password);
+    }
+
+    public void setMessage(String msg) {
+        lblInfo.setText(msg);
+        btnAuthenticate.setDisable(false);
+    }
+
+    @FXML
+    void initialize() {
+        assert btnAuthenticate != null : "fx:id=\"btnAuthenticate\" was not injected: check your FXML file 'LoginWindow.fxml'.";
+        assert lblInfo != null : "fx:id=\"lblInfo\" was not injected: check your FXML file 'LoginWindow.fxml'.";
+        assert pwdPassword != null : "fx:id=\"pwdPassword\" was not injected: check your FXML file 'LoginWindow.fxml'.";
+        assert txtUsername != null : "fx:id=\"txtUsername\" was not injected: check your FXML file 'LoginWindow.fxml'.";
+    }
+}
