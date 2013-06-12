1553e8e72f11ab532932234d75213277fa4505f4
Rick van Biljouw <rb10054603@gmail.com>
2013-06-12 12:38:29 +0200

    puuush

--- src/main/java/ms/aurora/sdn/net/api/ClientData.java
+++ src/main/java/ms/aurora/sdn/net/api/ClientData.java
@@ -8,10 +8,18 @@ import ms.aurora.sdn.net.packet.ClientDataRequest;
  * @author rvbiljouw
  */
 public class ClientData {
+    private static final Object dataLock = new Object();
     private static Plaintext data;
 
     public static void obtainData() {
         SDNConnection.getInstance().writePacket(new ClientDataRequest());
+        synchronized(dataLock) {
+            try {
+                dataLock.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
     }
 
     public static synchronized Plaintext getData() {
@@ -20,5 +28,8 @@ public class ClientData {
 
     public static synchronized void setData(Plaintext plaintext) {
         data = plaintext;
+        synchronized(dataLock) {
+            dataLock.notifyAll();
+        }
     }
 }
