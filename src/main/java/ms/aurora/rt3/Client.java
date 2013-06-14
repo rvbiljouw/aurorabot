7a55df83a032ce3582d378eb0995154bc87e2567
Rick van Biljouw <rvbiljouw@members.fsf.org>
2013-06-15 01:22:48 +0200

    AURCLI-2 #close Obsolete accessors and their references have been removed

--- src/main/java/ms/aurora/rt3/Client.java
+++ src/main/java/ms/aurora/rt3/Client.java
@@ -19,8 +19,6 @@ public interface Client {
 
     int getCameraPitch();
 
-    Widget[] getAllWidgets();
-
     String[] getMenuTargets();
 
     String[] getMenuActions();
@@ -91,11 +89,4 @@ public interface Client {
 
     Region[] getRegions();
 
-    Widget getInterface0();
-
-    Widget getInterface1();
-
-    Widget getInterface2();
-
-    Widget getInterface3();
 }
