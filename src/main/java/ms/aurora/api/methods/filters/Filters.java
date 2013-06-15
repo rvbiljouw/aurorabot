950686aa8009a8fbffc9176a06e2b1042957f3f3
Rick van Biljouw <rvbiljouw@aurora.ms>
2013-06-15 23:31:48 +0200

    AURCLI-3 #time 15m Added remaining filters, next up are Widget items, but i'm not quite sure what the best approach for doing those is.

--- src/main/java/ms/aurora/api/methods/filters/Filters.java
+++ src/main/java/ms/aurora/api/methods/filters/Filters.java
@@ -1,9 +1,8 @@
 package ms.aurora.api.methods.filters;
 
+import ms.aurora.api.methods.web.model.World;
 import ms.aurora.api.util.Predicate;
-import ms.aurora.api.wrappers.RSNPC;
-import ms.aurora.api.wrappers.RSObject;
-import ms.aurora.api.wrappers.RSTile;
+import ms.aurora.api.wrappers.*;
 
 /**
  * A class containing all kinds of filters
@@ -32,6 +31,23 @@ public final class Filters {
             }
         };
     }
+
+
+    /**
+     * a predicate which tests the location of the {@link ms.aurora.api.wrappers.RSObject} matches specified.
+     *
+     * @param location location that the {@link ms.aurora.api.wrappers.RSObject} should be on.
+     * @return true if the location matches the {@link ms.aurora.api.wrappers.RSObject} location.
+     */
+    public static Predicate<RSObject> OBJECT_LOCATION(final RSTile location) {
+        return new Predicate<RSObject>() {
+            @Override
+            public boolean apply(RSObject object) {
+                return object.getLocation().equals(location);
+            }
+        };
+    }
+
 
     /**
      * Creates a predicate that filters a set of npcs by the
@@ -76,16 +92,91 @@ public final class Filters {
     }
 
     /**
-     * a predicate which tests the location of the {@link ms.aurora.api.wrappers.RSObject} matches specified.
-     *
-     * @param location location that the {@link ms.aurora.api.wrappers.RSObject} should be on.
-     * @return true if the location matches the {@link ms.aurora.api.wrappers.RSObject} location.
+     * @param name the name of the {@link RSPlayer} you want to match
+     * @return true if the name of the {@link RSPlayer} matches the specified name
+     * @see ms.aurora.api.wrappers.RSPlayer#getName()
      */
-    public static Predicate<RSObject> OBJECT_LOCATION(final RSTile location) {
-        return new Predicate<RSObject>() {
+    public static Predicate<RSPlayer> PLAYER_NAME(final String name) {
+        return new Predicate<RSPlayer>() {
             @Override
-            public boolean apply(RSObject object) {
-                return object.getLocation().equals(location);
+            public boolean apply(RSPlayer object) {
+                return object.getName().equals(name);
+            }
+        };
+    }
+
+    /**
+     * tests of the current {@link ms.aurora.api.wrappers.RSCharacter] is not in combat
+     *
+     * @see ms.aurora.api.wrappers.RSNPC#isInCombat()
+     */
+    public static final Predicate<RSCharacter> NOT_IN_COMBAT = new Predicate<RSCharacter>() {
+        @Override
+        public boolean apply(RSCharacter object) {
+            return !object.isInCombat();
+        }
+    };
+
+    /**
+     * tests if the given {@link ms.aurora.api.wrappers.RSCharacter} is idle
+     *
+     * @see ms.aurora.api.wrappers.RSCharacter#getAnimation()
+     * @see ms.aurora.api.wrappers.RSCharacter#isInCombat()
+     * @see ms.aurora.api.wrappers.RSCharacter#isMoving()
+     */
+    public static final Predicate<RSCharacter> IDLE = new Predicate<RSCharacter>() {
+        @Override
+        public boolean apply(RSCharacter object) {
+            return object.getAnimation() == -1 && !object.isInCombat()
+                    && !object.isMoving();
+        }
+    };
+
+    /**
+     * Filters a ground item out by checking if it's ID matches any of the ones specified.
+     *
+     * @param ids A list of IDs that are accepted by the filter.
+     * @return predicate
+     */
+    public static Predicate<RSGroundItem> GROUND_ITEM_IDS(final int... ids) {
+        return new Predicate<RSGroundItem>() {
+            @Override
+            public boolean apply(RSGroundItem object) {
+                for (int id : ids) {
+                    if (object.getId() == id) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+
+
+    /**
+     * Filters a world based on it's world number
+     * @param worldNumber World number
+     * @return predicate
+     */
+    public static Predicate<World> WORLD_ID(final int worldNumber) {
+        return new Predicate<World>() {
+            @Override
+            public boolean apply(World object) {
+                return object.getWorldNo() == worldNumber;
+            }
+        };
+    }
+
+    /**
+     * Filters a world based on it's country name.
+     * @param countryName Country name
+     * @return predicate
+     */
+    public static Predicate<World> WORLD_COUNTRY(final String countryName) {
+        return new Predicate<World>() {
+            @Override
+            public boolean apply(World object) {
+                return object.getCountry().toLowerCase().contains(countryName.toLowerCase());
             }
         };
     }
