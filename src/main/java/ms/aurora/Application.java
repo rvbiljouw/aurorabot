6ffc2d0690f97af477b9e2d31f9e38f391951be0
Rick van Biljouw <rb10054603@gmail.com>
2013-06-26 13:35:42 +0200

    Logging

--- src/main/java/ms/aurora/Application.java
+++ src/main/java/ms/aurora/Application.java
@@ -36,7 +36,7 @@ public final class Application {
     public static JFrame mainFrame;
 
     public static void main(String[] args) {
-        logger.error("Testing error!");
+        logger.error("Testing error!", new RuntimeException("Hello!"));
         System.setSecurityManager(new DefaultSecurityManager());
         getDefaultToolkit().getSystemEventQueue().push(new GlobalEventQueue());
 
